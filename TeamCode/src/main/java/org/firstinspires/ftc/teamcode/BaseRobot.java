4b428947d42b38dcbbeb6266e924bf5d340cdbda
Scherrer <jacobscherrer29@marist.com>
2024-09-19 11:54:54 -0400

    tester teleop edits

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BaseRobot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BaseRobot.java
@@ -1,43 +1,73 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class BaseRobot {
 
-    DcMotor leftSlider;
-    DcMotor rightSlider;
+    DcMotor leftSlider;//from the perspective of the robot
+    DcMotor rightSlider;//from the perspective of the robot
     DcMotor pivotMotor;//worm gear box
     DcMotor hangArm;//monkey arm
+
 
 
 
     public BaseRobot(HardwareMap hwMap){
 
         hangArm = hwMap.dcMotor.get("hangArm");
-        hangArm.setPower(1);
+        //hangArm.setPower(1);
 
         pivotMotor = hwMap.dcMotor.get("pivotMotor");
-        pivotMotor.setPower(1);
+        //pivotMotor.setPower(1);
 
         leftSlider = hwMap.dcMotor.get("leftSlider");
-        leftSlider.setPower(1);
+        leftSlider.setDirection(DcMotorSimple.Direction.REVERSE);
 
-        leftSlider.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        leftSlider.setTargetPosition(100);
-        leftSlider.setPower(1);
+        //leftSlider.setPower(1);
+        rightSlider = hwMap.dcMotor.get("rightSlider");
+        //leftSlider.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        //leftSlider.setTargetPosition(100);
+        //leftSlider.setPower(1);
+
 
     }
 
-
-    public void sliderRunTo(int position){
+    public void sliderReset{
+        //make slider position 0
         leftSlider.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         rightSlider.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        leftSlider.setTargetPosition(position);
-        rightSlider.setTargetPosition(position);
-        leftSlider.setPower(1);
-        rightSlider.setPower(1);
+        leftSlider.setTargetPosition(0);
+        rightSlider.setTargetPosition(0);
+    }
+    public void slidesUp{
+        int slidesMax = 10;//change
+        leftSlider.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rightSlider.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        leftSlider.setTargetPosition(slidesMax);
+        leftSlider.setTargetPosition(slidesMax);
+    }
+    public void slidesDown{
+        leftSlider.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rightSlider.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        leftSlider.setTargetPosition(0);
+        leftSlider.setTargetPosition(0);
+    }
+    public void sub {
 
+        pivotMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        pivotMotor.setTargetPosition(0);//change
+        //set worm gear down and slides up
+    }
+    public void sliderRunTo(int position){
+        //leftSlider.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        //rightSlider.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        //leftSlider.setTargetPosition(position);
+        //rightSlider.setTargetPosition(position);
+        //leftSlider.setPower(1);
+        //rightSlider.setPower(1);
+;
 
 
     }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TesterTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TesterTeleop.java
@@ -1,17 +1,57 @@
 package org.firstinspires.ftc.teamcode;
 
+//import com.google.blocks.ftcrobotcontroller.runtime.CRServoAccess;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 @TeleOp
 public class TesterTeleop extends LinearOpMode {
+
+    BaseRobot robot;
+
+
+
     @Override
     public void runOpMode() throws InterruptedException {
+
+        robot = new BaseRobot(hardwareMap);
+
 
         waitForStart();
 
         while(!isStopRequested() && opModeIsActive()){
-            //code in here
+
+            //worm gear box
+            robot.pivotMotor.setPower(-gamepad1.left_stick_y);
+            //end worm gear box
+
+            //hang arm
+            if(gamepad1.y){
+                robot.hangArm.setPower(.2);
+            }
+            else if (gamepad1.a){
+                robot.hangArm.setPower(-.2);
+            }
+            //end hang arm
+
+            //slides
+            double TestSlidesPower = -gamepad1.right_stick_y;
+            robot.leftSlider.setPower(TestSlidesPower);
+            robot.rightSlider.setPower(TestSlidesPower);
+
+            //if (gamepad1.dpad_down){
+              //  slidesDown();
+            //}
+            //end slides
+
+
+            //servo test
+
+            //end servo test
+
+
+            //telemetry.addData("left stick: slides pivot motor");
+
         }
     }
 }
