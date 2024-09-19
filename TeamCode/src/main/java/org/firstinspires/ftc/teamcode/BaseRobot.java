0c2b0488f108c83297ebc717b1270ab64262c7f5
Scherrer <jacobscherrer29@marist.com>
2024-09-19 17:25:11 -0400

    added hold code to slides and hang arm

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BaseRobot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BaseRobot.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
 
 public class BaseRobot {
 
@@ -23,38 +24,44 @@ public class BaseRobot {
         //pivotMotor.setPower(1);
 
         leftSlider = hwMap.dcMotor.get("leftSlider");
-        leftSlider.setDirection(DcMotorSimple.Direction.REVERSE);
-
-        //leftSlider.setPower(1);
         rightSlider = hwMap.dcMotor.get("rightSlider");
-        //leftSlider.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        //leftSlider.setTargetPosition(100);
-        //leftSlider.setPower(1);
+        rightSlider.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        //Servo claw  = null;
+        //claw = hwMap.servo.get("claw");
+        //leftHand.setPosition(MID_SERVO);?????????
+
+
+
+
+
+
+
 
 
     }
 
-    public void sliderReset{
+    public void sliderReset() {
         //make slider position 0
         leftSlider.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         rightSlider.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         leftSlider.setTargetPosition(0);
         rightSlider.setTargetPosition(0);
     }
-    public void slidesUp{
+    public void slidesUp(){
         int slidesMax = 10;//change
         leftSlider.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         rightSlider.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         leftSlider.setTargetPosition(slidesMax);
         leftSlider.setTargetPosition(slidesMax);
     }
-    public void slidesDown{
+    public void slidesDown(){
         leftSlider.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         rightSlider.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         leftSlider.setTargetPosition(0);
         leftSlider.setTargetPosition(0);
     }
-    public void sub {
+    public void sub() {
 
         pivotMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         pivotMotor.setTargetPosition(0);//change
@@ -67,7 +74,7 @@ public class BaseRobot {
         //rightSlider.setTargetPosition(position);
         //leftSlider.setPower(1);
         //rightSlider.setPower(1);
-;
+
 
 
     }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TesterTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TesterTeleop.java
@@ -3,55 +3,111 @@ package org.firstinspires.ftc.teamcode;
 //import com.google.blocks.ftcrobotcontroller.runtime.CRServoAccess;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 @TeleOp
 public class TesterTeleop extends LinearOpMode {
 
     BaseRobot robot;
 
+    int leftSliderPos = 0;
+    int rightSliderPos = 0;
 
+    int hangArmPos = 0;
+
+    int pivotMotorPos = 0;
 
     @Override
     public void runOpMode() throws InterruptedException {
 
+
+
         robot = new BaseRobot(hardwareMap);
 
+        robot.hangArm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.hangArm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        hangArmPos = robot.hangArm.getCurrentPosition();
+
+        robot.leftSlider.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.leftSlider.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.rightSlider.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.rightSlider.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftSliderPos = robot.leftSlider.getCurrentPosition();
+        rightSliderPos = robot.rightSlider.getCurrentPosition();
+
+        robot.pivotMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.pivotMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        pivotMotorPos = robot.pivotMotor.getCurrentPosition();
 
         waitForStart();
 
         while(!isStopRequested() && opModeIsActive()){
+
+
 
             //worm gear box
             robot.pivotMotor.setPower(-gamepad1.left_stick_y);
             //end worm gear box
 
             //hang arm
-            if(gamepad1.y){
-                robot.hangArm.setPower(.2);
+            double hangArmMotorPower = gamepad1.right_trigger - gamepad1.left_trigger;
+            //int hangArmMotorDelta = (int) ()
+            if(hangArmMotorPower > .1){
+                hangArmPos += gamepad1.right_trigger * 10;
             }
-            else if (gamepad1.a){
-                robot.hangArm.setPower(-.2);
+            if(hangArmMotorPower < -.1){
+                hangArmPos -= gamepad1.right_trigger * 10;
             }
             //end hang arm
 
-            //slides
-            double TestSlidesPower = -gamepad1.right_stick_y;
-            robot.leftSlider.setPower(TestSlidesPower);
-            robot.rightSlider.setPower(TestSlidesPower);
+            int  slidesMotorDelta = (int) (gamepad1.right_stick_y * 10);
 
-            //if (gamepad1.dpad_down){
-              //  slidesDown();
-            //}
-            //end slides
+            if(slidesMotorDelta > 1){
+                leftSliderPos += slidesMotorDelta;
+                rightSliderPos += slidesMotorDelta;
+            }
+
+            if (slidesMotorDelta < -1){
+                leftSliderPos -= slidesMotorDelta;
+                rightSliderPos -= slidesMotorDelta;
+            }
+
+            robot.leftSlider.setTargetPosition(leftSliderPos);
+            robot.rightSlider.setTargetPosition(rightSliderPos);
+
+            robot.leftSlider.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            robot.rightSlider.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            robot.leftSlider.setPower(1);
+            robot.rightSlider.setPower(1);
+
+            if(rightSliderPos < 0){
+                rightSliderPos = 0;
+            }
+            if(leftSliderPos < 0){
+                leftSliderPos = 0;
+            }
+            if(leftSliderPos > 3000) {//change number
+                //leftSliderPos = 3000 change number
+            }
+            if(rightSliderPos > 3000) {//change number
+                //leftSliderPos = 3000 change number
+            }
 
 
-            //servo test
+            if (gamepad1.dpad_down){
+                //slidesDown(); //sets slides position to 0
+            }
 
-            //end servo test
 
 
-            //telemetry.addData("left stick: slides pivot motor");
+            telemetry.addData("left slides position: ", leftSliderPos);
+            telemetry.addData("hang arm position: ", hangArmPos);
+            telemetry.addData("right slides position: ", rightSliderPos);
+            telemetry.addData("pivot motor position: ", pivotMotorPos);
+            telemetry.addData("Slides motor delta: ", slidesMotorDelta);
 
+            telemetry.update();
         }
     }
 }
